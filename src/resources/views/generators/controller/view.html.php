60925510c007973f6feeccebd3266a38be6adfe7
owner <owner@localhost>
2026-03-19 11:55:01 +0900

    テンプレート変更

--- src/resources/views/generators/controller/view.html.php
+++
@@ -1,4 +0,0 @@
-<h2 class="app-h2"><?= $data['viewPrefix'] ?>.<?= $data['action'] ?></h2>
-
-<div>
-</div>
---
+++ src/resources/views/generators/controller/view.sfw.php
@@ -0,0 +1,4 @@
+<h2 class="app-h2">{{ $data['viewPrefix'] }}.{{ $data['action'] }}</h2>
+
+<div>
+</div>
