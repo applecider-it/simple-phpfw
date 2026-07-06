cc1b9c6517a7fd7c3208b32a5c00f17d87d99513
owner <owner@localhost>
2026-07-06 19:17:19 +0900

    デザイン

--- src/resources/views/partials/message/loading.html.php
+++ src/resources/views/partials/message/loading.html.php
@@ -4,6 +4,6 @@
   :class="show ? 'opacity-100' : ''"
   class="flex justify-center items-center h-52 text-gray-500 font-sans text-lg opacity-0 transition-opacity duration-500 ease-in"
 >
-    <div class="w-8 h-8 border-4 border-gray-300 border-t-gray-700 rounded-full animate-spin mr-2"></div>
+    <div class="w-8 h-8 border-2 border-gray-300 border-t-gray-700 rounded-full animate-spin mr-2"></div>
     <div>読み込み中…</div>
 </div>
