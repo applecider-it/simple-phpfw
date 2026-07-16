c9ab878e03e0d62958a6f099a3797368007e4ca8
owner <owner@localhost>
2026-07-16 15:09:36 +0900

    開発者向けページ

--- src/resources/views/development/javascript_test.html.php
+++ src/resources/views/development/javascript_test.html.php
@@ -15,24 +15,27 @@ $list = [
 <?= $vite->importJs('resources/js/entrypoints/development/javascript-test.ts') ?>
 
 <h2 class="app-h2">development.javascript_test</h2>
-<div>
+<div class="space-y-4">
     <div id="vue"
         data-all="<?= $this->h(json_encode([
                         'valueTest' => compact('value1'),
                     ])) ?>">
     </div>
 
-    <div class="swiper-container swiper-container1">
-        <div class="swiper swiper1">
-            <div class="swiper-wrapper">
-                <?php foreach ($list as $val): ?>
-                    <div class="swiper-slide">
-                        <Image src="<?= $this->h($val) ?>" alt="" class="mx-auto" />
-                    </div>
-                <?php endforeach; ?>
-            </div>
+    <div>
+        <h3 class="app-h3">swiper</h3>
+        <div class="swiper-container swiper-container1">
+            <div class="swiper swiper1">
+                <div class="swiper-wrapper">
+                    <?php foreach ($list as $val): ?>
+                        <div class="swiper-slide">
+                            <Image src="<?= $this->h($val) ?>" alt="" class="mx-auto" />
+                        </div>
+                    <?php endforeach; ?>
+                </div>
 
-            <div class="swiper-pagination swiper-pagination1"></div>
+                <div class="swiper-pagination swiper-pagination1"></div>
+            </div>
         </div>
     </div>
 </div>
